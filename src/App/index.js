9ce19225bd90b83c9fda1b7e1c84bf7e461f2f30
Andres Pulgarin <andres.pulgarin.nopia@gmail.com>
2024-03-28 17:11:34 -0500

    implementacion de render function en TodoList

--- src/App/index.js
+++ src/App/index.js
@@ -46,9 +46,13 @@ function App() {
         error={error}
         loading={loading}
         searchedTodos={searchedTodos}
+        totalTodos={totalTodos}
+        searchText={searchValue}
         onError={() => <TodosError />}
         onLoading={() => <TodosLoading />}
-        onEmpty={() => <EmptyTodos />}
+        onEmptyTodos={() => <EmptyTodos />}
+        onEmptySearchResults={
+          (searchText) => <p>No hay resultados para {searchText} </p>}
         render={todo => (
           <TodoItem
             key={todo.text}
@@ -58,9 +62,19 @@ function App() {
             onDelete={() => deleteTodo(todo.text)}
           />
         )}
-      />
+      > 
+      {/* {todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        )} */}
+      </TodoList>
 
-      <TodoList>
+      {/* <TodoList> */}
         {/* {error && <TodosError />}
         {loading && <TodosLoading />}
         {(!loading && !searchedTodos.length) && <EmptyTodos />}
@@ -74,7 +88,7 @@ function App() {
             onDelete={() => deleteTodo(todo.text)}
           />
         ))} */}
-      </TodoList>
+      {/* </TodoList> */}
 
       {!!openModal && (
         <Modal>
