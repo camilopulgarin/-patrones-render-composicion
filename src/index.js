76c1e67e84f75a1c1b4f2be1694ecfa1e1b7a396
Andres Pulgarin <andres.pulgarin.nopia@gmail.com>
2024-03-30 14:33:34 -0500

    implementacion de HOCs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/index.js';
+// import App from './App/index.js';
 import './index.css';
 
+function App(props) {
+  return(
+    <h1>{props.saludo}, {props.nombre},</h1>
+  )
+}
+
+function withSaludo(WrappedComponent) {
+  return function WrappedComponentWthSaludo(saludo) {
+    return function ComponenteDeVerdad(props) {
+      return (
+        <React.Fragment>
+          <WrappedComponent {...props} saludo={saludo}/>
+          <p>Estamos acompañando al wrapped</p>
+        </React.Fragment>
+      )
+    }
+  }
+}
+
+const AppWithSaludo = withSaludo(App)("Wenas")
+
 ReactDOM.render(
-  <App />,
+  <AppWithSaludo nombre="Vane" />,
+  //  <App saludo="Buenas" nombre="Vane"/>,
   document.getElementById('root')
 );
+
+
